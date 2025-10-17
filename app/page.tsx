7b471cc6daebe47c771e3d67493ef45896cd6095
isat83 <tisat@yandex.ru>
2025-10-17 16:03:02 +0300

    🚑 Fix: add HeroBlock and BiographyBlock after rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import HeroSection from '@/components/HeroSection'
-import BiographyTeaser from '@/components/BiographyTeaser'
+import HeroBlock from "@/components/HeroBlock";
+import BiographyBlock from "@/components/BiographyBlock";
 
 export default function Home() {
   return (
